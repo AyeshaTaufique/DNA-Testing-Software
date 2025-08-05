2820dca9c30c504cc5ccd1e4f8cb72f379733cb3
AyeshaTaufique <148105714+AyeshaTaufique@users.noreply.github.com>
2025-08-05 11:26:08 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
-
+from .views import test_full_report
+from .views import run_rmp_test
 
 
 
@@ -10,20 +10,34 @@ urlpatterns = [
     path('logout/', views.logout_chemist, name='logout'),
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+
     path('dashboard/', views.dashboard, name='dashboard'),
+
     path('chemist-profile/', views.chemist_profile, name='chemist_profile'),
     path('update-chemist-profile/', views.update_chemist_profile, name='update_chemist_profile'),
+
     path('add-profile/', views.add_profile, name='add_profile'),
     path('save-profile/', views.save_profile, name='save_profile'),
+
     path('get-dna-profiles/', views.get_dna_profiles, name='get_dna_profiles'),
+
     path('duo-test/', views.duo_test, name='duo_test'),
     path('trio-test/', views.trio_test, name='trio_test'),
     path('complex-test/', views.complex_test, name='complex_test'),
     path('kinship-analysis/', views.kinship_analysis, name='kinship_analysis'),
     path('sibship-analysis/', views.sibship_analysis, name='sibship_analysis'),
     path('profile-inclusion/', views.profile_inclusion, name='profile_inclusion'),
+    
     path('random-match/', views.random_match, name='random_match'),
+    path('run-rmp-test/', views.run_rmp_test, name='run_rmp_test'),
+    path('run-duo-paternity-test/', views.run_duo_paternity_test, name='run_duo_paternity_test'),
+    path('run-trio-paternity-test/', views.run_trio_paternity_test, name='run_trio_paternity_test'),
+
+
+
     path('report/', views.report, name='report'),
+    path('report/<int:test_id>/', views.test_full_report, name='test_result'),
+
 
 
 ]
